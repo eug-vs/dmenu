da5301964aba42e48e15fb7c95614f2fd7e8fed3
eug-vs <eugene@eug-vs.xyz>
2022-04-16 15:57:50 +0300

    feat: use gray background

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#689d6a" },
+	[SchemeSel] = { "#eeeeee", "#928374" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
